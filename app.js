3ee963ef287fccaf29ff6e284dd60d789df350b6
28JayG <jaygandhi.32@outlook.com>
2022-05-08 05:39:24 +0530

    adds config.env

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tour.route')
-const userRouter = require('./routes/user.route')
+const tourRouter = require('./routes/tour.route');
+const userRouter = require('./routes/user.route');
 
 const app = express();
 
 //middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-
+app.use(express.static(` ${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -21,8 +22,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
-
-
-
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`app running on port: ${PORT}`));
