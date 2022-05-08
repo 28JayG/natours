c3db7eda8f80bf74a8c1282c6b8495e1e13024d2
28JayG <jaygandhi.32@outlook.com>
2022-05-08 05:41:46 +0530

    hotifx

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`app running on port: ${PORT}`));
