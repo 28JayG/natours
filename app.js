4a4ae2e13fb4d52b7f572aa1f5d0d0d0d64ca97a
28JayG <jaygandhi.32@outlook.com>
2022-07-22 11:12:26 +0530

    adds sanitization, xss, helmet, ecuirty and stuff

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/app-error');
 const globalErrorHandler = require('./controllers/error.controllers');
@@ -7,14 +12,35 @@ const tourRouter = require('./routes/tour.route');
 const userRouter = require('./routes/user.route');
 
 const app = express();
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Limit for requests passed, try again after 1 hour!',
+});
 
 //middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+//set security limit to api calling
+app.use('/api', limiter);
+//set security http header
+app.use(helmet());
+//reading data from body
 app.use(express.json());
+//data sanitization against noSQL query injection
+app.use(mongoSanitize());
+// data sanitization against XSS
+app.use(xss());
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ['duration' ],
+  })
+);
+//serving static files
 app.use(express.static(` ${__dirname}/public`));
-
+//custom middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
