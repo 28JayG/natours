828336a651cc3eba6327d0a12603ec4ecad270c9
28JayG <jaygandhi.32@outlook.com>
2022-07-24 23:01:53 +0530

    data importing for users & reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ dotenv.config({ path: './config.env' });
 
 const mongoose = require('mongoose');
 const Tour = require('../../model/tours.model');
+const User = require('../../model/users.model');
+const Review = require('../../model/reviews.modal');
 
 const DB = process.env.DB_CONNECT_STRING.replace(
   '<PASSWORD>',
@@ -16,13 +18,17 @@ mongoose.connect(DB, {
   useFindAndModify: false,
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
 );
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     process.exit();
   } catch (err) {
     console.log(err);
@@ -33,6 +39,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     process.exit();
   } catch (err) {
     console.log(err);
